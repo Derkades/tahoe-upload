c353673706eb5e0ea2a24ebbdf332a06637ed74a
Robin <robinslot@hotmail.nl>
2021-11-29 18:51:36 +0100

    Make one request for the entire directory contents
    Massive performance improvement when an entire directory has already been uploaded

--- upload.py
+++ upload.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import sys
 import requests
-from typing import BinaryIO, Generator
+from typing import BinaryIO, Generator, Set, Dict, Tuple
 from urllib.parse import quote
 from pathlib import Path
 from argparse import ArgumentParser
@@ -13,6 +13,33 @@ UPLOAD_BLOCK_SIZE = 512*1024
 UPLOAD_PROGRESS_UNIT = 'bytes'
 assert UPLOAD_BLOCK_SIZE > 0
 assert UPLOAD_PROGRESS_UNIT in {'bytes', 'bits'}
+
+
+def get_names_in_dircap(api: str, parent_cap: str) -> Dict[str, Tuple[str, bool, int]]:
+    r = requests.get(f'{api}/uri/{quote(parent_cap)}?t=json',
+                     headers={'Accept': 'text/plain'})
+    assert r.status_code == 200
+    (node_type, content) = r.json()
+    assert node_type == 'dirnode'
+
+    children = {}
+
+    for child_name, child_content in content['children'].items():
+        child_type = child_content[0]
+        if child_type == 'dirnode':
+            cap = child_content[1]['rw_uri']
+            is_dir = True
+            size = None
+        elif child_type == 'filenode':
+            cap = child_content[1]['ro_uri']
+            is_dir = False
+            size = child_content[1]['size'] if 'size' in child_content[1] else None
+        else:
+            raise Exception('Unsupported node type ' + child_type)
+
+        children[child_name] = (cap, is_dir, size)
+
+    return children
 
 
 def file_reader(f: BinaryIO, bar: tqdm) -> Generator[bytes, None, None]:
@@ -40,31 +67,28 @@ def upload_file(path: Path, file_size: int, api: str, parent_cap: str, log_prefi
         print(log_prefix + path.name, 'done!')
     else:
         print(r.text)
-        print('Failed to upload file ' + str(path))
+        print(f'Failed to upload file {path}, status code {r.status_code}')
         exit(1)
 
 
-def check_upload_file(path: Path, api: str, parent_cap: str, log_prefix: str) -> None:
+def check_upload_file(path: Path, api: str, parent_cap: str, parent_contents: Dict[str, Tuple[str, bool, int]], log_prefix: str) -> None:
     # check if a file or directory with this name already exists
     print(log_prefix + path.name, end=': ')
-    r = requests.get(f'{api}/uri/{quote(parent_cap)}/{quote(path.name)}?t=json',
-                     headers={'Accept': 'text/plain'})
-    if r.status_code == 200:
+    if path.name in parent_contents:
         print('already exists, ', end='', flush=True)
-        json = r.json()
-
-        if json[0] == 'filenode':
-            if json[1]['format'] == 'CHK':
-                file_size = path.stat().st_size
-                if json[1]['size'] == file_size:
-                    print('same size',)
+        (_cap, is_dir, size) = parent_contents[path.name]
+        local_size = path.stat().st_size
+        if not is_dir:
+            if size is not None:
+                if size == local_size:
+                    print('same size')
                     return
                 else:
                     print('different size', end=' ', flush=True)
             else:
-                print('different format', json[1]['format'], end=' ', flush=True)
+                print('file has no size, mutable?', end=' ', flush=True)
         else:
-            print('unexpected node type (probably a directory)', end=' ', flush=True)
+            print('remote cap is a directory', end=' ', flush=True)
 
         print('deleting...', end=' ', flush=True)
         r = requests.delete(f'{api}/uri/{quote(parent_cap)}/{quote(path.name)}',
@@ -74,51 +98,40 @@ def check_upload_file(path: Path, api: str, parent_cap: str, log_prefix: str) ->
             print(r.text)
             exit(1)
         print('re-uploading...')
-        upload_file(path, file_size, api, parent_cap, log_prefix)
-    elif r.status_code == 404:
+        upload_file(path, local_size, api, parent_cap, log_prefix)
+    else:
         print('uploading...')
         upload_file(path, path.stat().st_size, api, parent_cap, log_prefix)
-    else:
-        print(r.text)
-        print('Unexpected status code ' + str(r.status_code))
-        exit(1)
 
 
-def upload_dir(path: Path, api: str, parent_cap: str, log_prefix: str) -> None:
+def upload_dir(path: Path, api: str, parent_cap: str, parent_contents: Dict[str, Tuple[str, bool, int]], log_prefix: str) -> None:
     print(log_prefix + path.name, end=': ')
-    r = requests.get(f'{api}/uri/{quote(parent_cap)}/{quote(path.name)}?t=json',
-                     headers={'Accept': 'text/plain'})
-    if r.status_code == 200:
-        json = r.json()
-        if json[0] != 'dirnode':
+    if path.name in parent_contents:
+        (cap, is_dir, _size) = parent_contents[path.name]
+        if not is_dir:
             print('not a directory in tahoe filesystem!')
+            # TODO Delete remote file and create remote directory if not a dir
             exit(1)
-        else:
-            cap = json[1]['rw_uri']
-            cap_type = cap.split(':')[1]
-            assert cap_type == 'DIR2'
-            print('already exists, ', end='', flush=True)
-    elif r.status_code == 404:
+        print('already exists, ', end='', flush=True)
+    else:
         print('creating directory...', end=' ', flush=True)
         r = requests.post(f'{api}/uri/{quote(parent_cap)}/{quote(path.name)}?t=mkdir',
                           headers={'Accept': 'text/plain'})
         cap = r.text
         print('created,', end=' ', flush=True)
-    else:
-        print(r.text)
-        print('Unexpected status code ' + str(r.status_code))
-        exit(1)
 
     print('uploading contents....')
     upload_contents(parent_path=path, api=api, parent_cap=cap, log_prefix=(log_prefix + '    '))
 
 
 def upload_contents(parent_path: Path, api: str, parent_cap: str, log_prefix: str) -> None:
+    parent_contents = get_names_in_dircap(api, parent_cap)
+
     for path in parent_path.iterdir():
         if path.is_file():
-            check_upload_file(path, api, parent_cap, log_prefix)
+            check_upload_file(path, api, parent_cap, parent_contents, log_prefix)
         elif path.is_dir():
-            upload_dir(path, api, parent_cap, log_prefix)
+            upload_dir(path, api, parent_cap, parent_contents, log_prefix)
         else:
             print(log_prefix + path.name, "skipping, unknown file type")
 
